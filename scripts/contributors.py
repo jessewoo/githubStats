be7f5ca0092bcb5fa278ebfc20e1cf7a4a60a4ea
Jesse Woo <woojesse@gmail.com>
2022-10-27 15:00:03 -0700

    request get

--- scripts/contributors.py
+++ scripts/contributors.py
@@ -1,1 +1,6 @@
-print('contributors')
+import requests
+response = requests.get("https://api.github.com/repos/hubzero/hubzero-cms/contributors")
+data = response.json()
+
+number_of_contributors = len(data)
+print("Number of contributors: ", number_of_contributors)
