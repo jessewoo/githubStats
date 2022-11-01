bb6f826aa37afe2b1f711778c0809d5eaab305bb
Jesse Woo <woojesse@gmail.com>
2022-11-01 12:10:31 -0700

    geolocate

---
+++ geo/geolocate.py
@@ -0,0 +1,6 @@
+# RESOURCES
+
+# https://blog.reconinfosec.com/visualizing-geo-ip-information-in-python
+# https://www.abstractapi.com/guides/google-map-ip-address-geolocation
+# https://docs.splunk.com/Documentation/Splunk/9.0.1/Viz/IplocationChoropleth
+# https://stackoverflow.com/questions/1565555/plot-geoip-data-on-a-world-map
--- scripts/commits.py
+++ scripts/commits.py
@@ -4,21 +4,27 @@ import requests
 
 print("---- FEATURE - GIT COMMITS IN THE LAST YEAR ----\n")
 
+
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+
 # Returns the total commit counts for the owner and total commit counts in all in the last 52 weeks.
-commit_response = requests.get("https://api.github.com/repos/hubzero/hubzero-cms/stats/participation")
+commit_response = requests.get(
+    "https://api.github.com/repos/hubzero/hubzero-cms/stats/participation")
 commit_data = commit_response.json()
-each_week_data = commit_data["all"];
+
+print("Output: ", commit_data)
+
+each_week_data = commit_data["all"]
 number_of_weeks = len(each_week_data)
 # print("Number of weeks: ", number_of_weeks)
 
 chunk = list(chunks(each_week_data, 4))
 print("** Commits in the last 52 weeks, segmented by months")
-print(chunk,"\n")
+print(chunk, "\n")
 
 print("** Commits in the last 12 months")
 chunk_by_month = [sum(l) for l in chunk]
