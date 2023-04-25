371ef5f24b01ffac15bb3dea4f4d0856dc1dcf0a
Jesse Woo <woojesse@gmail.com>
2023-04-25 13:48:05 -0700

    updates on redistribution

--- scripts/distributeSize.py
+++ scripts/distributeSize.py
@@ -2,19 +2,15 @@ import random
 
 print("---- DISTRIBUTE AN ARRAY OF COUNTS ----\n")
 
-maxCount = 5;
+maxCount = 10;
 
 def redistributeToMaxCount(list):
     listMax = max(list)
-
     return [((x / listMax) * maxCount) for x in list]
 
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-list5 = [random.randint(0,10) for i in range(5)]
+list5 = [random.randint(0,100) for i in range(10)]
 list5max = max(list5)
-newList = [((x / list5max) * maxCount) for x in list5]
+newList = redistributeToMaxCount(list5)
 
 print(list5)
 print(newList)
